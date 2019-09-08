2ae572c465a5aa59c9d112475a0d33212036fcd5
mdhossain <alamgirhossain1984@gmail.com>
2019-09-08 12:30:49 -0400

    Math problem

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -12,5 +12,29 @@ public class Factorial {
          *
          */
 
+
+        System.out.println("Factorial of 5 by using Recurtion: "+factorialRecursive(5));
+        System.out.println("Factorial of 5 by using Iteration: "+factorialIterative(5));
     }
+    //using recurtion
+    public static int factorialRecursive(int n){
+        if(n <= 1){
+            return 1;
+        }
+        else{
+            return n*factorialRecursive(n-1);
+        }
+    }
+    //using iteration
+    public static int factorialIterative(int n){
+        int result=1,i=1;
+        while(i<=n){
+            result=result*i;
+            i++;
+        }
+
+        return result;
+
+    }
+
 }
--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -7,6 +7,18 @@ public class Fibonacci {
           0,1,1,2,3,5,8,13
          */
 
+         int fib []= new int[40];
+        for(int i=0;i<40;i++){
+            if (i<2){
+                fib[i]=i;
+            }else{
+                fib[i]=fibo[i-1]+fib[i-2];
+            }
+
+
+        }
+        System.out.println(Arrays.toString(fib));
+    }
 
     }
 }
